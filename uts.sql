5452565a7686f33c3f669cd08048e09b2f381faf
KHAIRATUN HI'ISAN <72597568+KHAIRATUN514@users.noreply.github.com>
2021-11-15 03:42:13 +0700

    Add files via upload

---
+++ uts.sql
@@ -0,0 +1,72 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 14, 2021 at 09:36 PM
+-- Server version: 10.4.14-MariaDB
+-- PHP Version: 7.2.33
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `uts`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tmhs`
+--
+
+CREATE TABLE `tmhs` (
+  `id_mhs` int(11) NOT NULL,
+  `nim` varchar(9) NOT NULL,
+  `nama` varchar(100) NOT NULL,
+  `alamat` varchar(128) NOT NULL,
+  `prodi` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tmhs`
+--
+
+INSERT INTO `tmhs` (`id_mhs`, `nim`, `nama`, `alamat`, `prodi`) VALUES
+(363, '202005110', 'Agus', 'Klaten', 'S1-Sistem Informasi'),
+(364, '202005122', 'Feby', 'SAMPIT', 'S1-Manajemen Informasi'),
+(365, '202005101', 'LARAS', 'bogor', 'S1-Manajemen Informasi'),
+(366, '202005110', 'Putra', 'Bontang', 'S1-Manajemen Informasi'),
+(367, '202005110', 'Putri', '', 'S1-Teknik Informatika');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tmhs`
+--
+ALTER TABLE `tmhs`
+  ADD PRIMARY KEY (`id_mhs`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tmhs`
+--
+ALTER TABLE `tmhs`
+  MODIFY `id_mhs` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=368;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
